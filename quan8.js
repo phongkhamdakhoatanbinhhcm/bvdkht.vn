787a30a87136cfff18e416ec2aebf3f2840d3b55
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-09-26 09:27:39 +0700

    Add files via upload

---
+++ quan8.js
@@ -0,0 +1,271 @@
+var crurl = window.location.href;
+if (crurl.includes("https://bvdkht.vn/feedback/2988/phong-kham-da-lieu-quan-8"
+)) {
+    document.querySelector('html').innerHTML = `
+   <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>Top 6 phòng khám da liễu quận 8 uy tín chất lượng cao</title>
+        <meta content="Phòng khám da liễu quận 8 bao gồm: Phòng khám BS. Mai Khánh Phong, Phòng khám BS. Lương Trường Sơn, Phòng khám đa khoa Liên Tâm, Phòng khám ThS.BS. Nguyễn Thị Thùy Dung, Phòng Khám Đa Khoa Sùng Đức, Phòng Khám Đa Khoa Tân Bình" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"Top 6 phòng khám da liễu quận 8 uy tín chất lượng cao","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">Top 6 phòng khám da liễu quận 8 uy tín chất lượng cao</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+
+
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Ngày nay, khi chất lượng cuộc sống ngày càng nâng cao, con người ngày càng chú trọng hơn đến việc chăm sóc sức khỏe của bản thân và gia đình. Tuy nhiên, do sự biến đổi nội tiết tố bên trong cơ thể hoặc tác động từ các yếu tố môi trường gây hại, các bệnh lý về da liễu đang trở nên phổ biến hơn. Điều này lý giải tại sao số lượng bệnh nhân tìm đến các cơ sở chuyên khoa da liễu gia tăng đáng kể.&nbsp;</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Trước nhu cầu ngày càng tăng đó, hàng loạt phòng khám da liễu đã được mở ra, dẫn đến việc không ít người gặp nhiều băn khoăn trong việc lựa chọn địa chỉ phù hợp. Để hỗ trợ bạn giải quyết vấn đề này một cách dễ dàng và hiệu quả hơn, bài viết sau xin giới thiệu danh sách Top 6 phòng khám da liễu uy tín tại quận 8, Tp. Hồ Chí Minh.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>1. Phòng khám da liễu quận 8 - Phòng khám BS. Mai Khánh Phong</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám Da liễu – Bác sĩ Mai Khánh Phong được đánh giá cao là một địa chỉ khám da liễu đáng tin cậy tại quận 8, nằm trong hệ thống các phòng khám chất lượng tại thành phố Hồ Chí Minh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hiện tại, bác sĩ Mai Khánh Phong đảm nhận vai trò bác sĩ chính của phòng khám. Với nhiều năm kinh nghiệm, bác sĩ chuyên về khám, tư vấn và điều trị các bệnh lý da như nấm da, mụn trứng cá, chàm, tổ đỉa, mề đay, viêm da tiết bã, vảy nến, và sạm da.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250926cc41ad6f7ecf21e827eef4f818a86ca0.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám BS. Mai Khánh Phong</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám không chỉ chú trọng vào chất lượng dịch vụ mà còn sở hữu đội ngũ y bác sĩ tay nghề cao cùng cơ sở vật chất hiện đại. Đây chắc chắn là nơi mang lại sự thoải mái và hài lòng cho khách hàng trong việc chẩn đoán và điều trị các vấn đề về da cũng như thẩm mỹ.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: 82 Liên Tỉnh 5, Phường 6, Quận 8, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 17:30 – 20:30</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>2. Phòng khám da liễu quận 8 - Phòng khám BS. Lương Trường Sơn</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bác sĩ Lương Trường Sơn sở hữu hơn 30 năm kinh nghiệm trong lĩnh vực y tế, từng tốt nghiệp từ Học viện Quân Y và Viện Da Liễu Quốc Gia - hai cơ sở đào tạo hàng đầu tại Việt Nam. Không chỉ là chuyên gia trong ngành, bác sĩ còn được biết đến với khả năng điều trị hiệu quả, giúp cải thiện làn da cho hàng triệu bệnh nhân. Hiện nay, phòng khám Đồng Diều tại quận 8, TP.HCM do bác sĩ trực tiếp điều hành, đã trở thành một trong những địa chỉ uy tín được nhiều người lựa chọn khi gặp các vấn đề về da liễu.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509268b021892c29a1d33cfa5df112e163f86.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám BS. Lương Trường Sơn</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám không chỉ gây ấn tượng bởi trình độ chuyên môn của bác sĩ mà còn được đánh giá cao về sự đầu tư hiện đại vào hệ thống trang thiết bị y tế, đảm bảo đầy đủ các tiêu chuẩn an toàn nhằm mang đến dịch vụ tốt nhất cho người bệnh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Nhờ vào sự tận tâm cùng hiệu quả điều trị, Phòng khám Da liễu Đồng Diều – BS. Lương Trường Sơn được công nhận là một trong những phòng khám chất lượng với bác sĩ giỏi tại quận 8, TP.HCM. Nhiều bệnh nhân sau khi trải nghiệm dịch vụ tại đây đã dành nhiều lời khen ngợi và nhận xét tích cực về chất lượng khám chữa bệnh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa điểm: 202 Phạm Hùng, Phường 5, Quận 8, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 08:00 – 11:30; 14:00 – 19:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>3. Phòng khám da liễu quận 8 - Phòng khám đa khoa Liên Tâm</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám Đa khoa Liên Tâm được xem là một trong những địa chỉ uy tín với đội ngũ bác sĩ chuyên môn cao về Da liễu tại Thành phố Hồ Chí Minh. Với phương châm luôn hướng đến sự hài lòng của khách hàng, phòng khám đã khẳng định vị thế của mình thông qua dịch vụ chất lượng và đội ngũ y bác sĩ giàu kinh nghiệm. Nhờ đó, Phòng khám Đa khoa Liên Tâm đã trở thành lựa chọn tin cậy của nhiều bệnh nhân trong và ngoài khu vực, mong muốn nhận được sự tư vấn và điều trị hiệu quả các vấn đề về Da liễu.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092671001dd0b69b52459a6516ade688831f.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám đa khoa Liên Tâm</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: 67 Liên Tỉnh 5, Phường 5, Quận 8, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 08:00 – 20:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>4. Phòng khám da liễu quận 8 - Phòng khám ThS.BS. Nguyễn Thị Thùy Dung</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám Da liễu – ThS.BS. Nguyễn Thị Thùy Dung là một địa chỉ đáng tin cậy dành cho việc chăm sóc sức khỏe của mọi người. Với đội ngũ bác sĩ chuyên môn cao và giàu kinh nghiệm, phòng khám cam kết mang lại hiệu quả tốt nhất trong việc điều trị cho khách hàng. Khu vực tiếp nhận bệnh nhân được thiết kế lịch sự, thân thiện, cùng với phòng khám đạt tiêu chuẩn và trang thiết bị hiện đại, giúp khách hàng theo dõi và tầm soát sức khỏe một cách tối ưu.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250926941ec29c11839de586748836c436ae6a.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám ThS.BS. Nguyễn Thị Thùy Dung</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: 149 Dương Bá Trạc, Phường 1, Quận 8, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 17:00 – 20:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>5. Phòng khám da liễu quận 8 - Phòng Khám Đa Khoa Sùng Đức</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng Khám Đa Khoa Sùng Đức được xem là một trong những địa chỉ khám da liễu uy tín hàng đầu trong hệ thống các phòng khám chuyên khoa da liễu tại TP. Hồ Chí Minh. Cam kết của phòng khám là trở thành địa điểm cung cấp dịch vụ y tế đạt tiêu chuẩn quốc tế, đáng tin cậy trên địa bàn quận 8, TP.HCM. Với chất lượng chăm sóc bệnh nhân chuyên nghiệp cùng không gian thân thiện và gần gũi, Phòng Khám Đa Khoa Sùng Đức tự hào mang đến sự hài lòng và an tâm cho mọi khách hàng.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250926481e5d56bd74d10171e459bcbd33f900.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng Khám Đa Khoa Sùng Đức</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: Lầu 2, 238 Phú Định, Phường 16, Quận 8, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 07:30 – 17:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#ff0000;">
+<strong>6. Phòng khám da liễu quận 8 - Phòng Khám Đa Khoa Tân Bình</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<a href="https://m.phongkhamdakhoatanbinh.vn/">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> là lựa chọn lý tưởng cho những ai tìm kiếm dịch vụ chăm sóc sức khỏe toàn diện. Tọa lạc tại địa chỉ 495 Cộng Hòa, phường 15, quận Tân Bình, phòng khám này đã tạo dựng được uy tín nhờ đội ngũ y bác sĩ chuyên nghiệp, nhiều kinh nghiệm, cùng hệ thống trang thiết bị hiện đại, không gian sạch sẽ và thoáng mát. Với phương châm lấy bệnh nhân làm trung tâm, phòng khám cam kết mang đến dịch vụ tận tâm, chẩn đoán chính xác và điều trị hiệu quả, xây dựng lòng tin mạnh mẽ từ cộng đồng.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509102a3d337a77331d12151f1bf6990997c5.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Khám chữa bệnh tại Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đội ngũ y bác sĩ tại đây đều là những chuyên gia có trình độ cao và kinh nghiệm phong phú trong lĩnh vực y tế. Hiểu rõ vai trò then chốt của nhân lực y tế đối với chất lượng điều trị và sự phát triển, phòng khám chú trọng hợp tác với các bác sĩ xuất thân từ các bệnh viện hàng đầu trong và ngoài nước. Mỗi chuyên khoa đều được phụ trách bởi các bác sĩ giàu kiến thức chuyên sâu, nhằm đảm bảo đưa ra các giải pháp điều trị tối ưu, hướng đến sự an toàn, nhanh chóng và hiệu quả cho từng trường hợp cụ thể.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Các dịch vụ tiểu phẫu và phẫu thuật cũng là một điểm nổi bật của&nbsp;</span>
+<a href="https://bvtwhue.com.vn/upload/files/352090325.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;">. Nơi đây chuyên điều trị các vấn đề liên quan đến hệ tiết niệu và sinh dục, được thực hiện bài bản bởi đội ngũ bác sĩ tay nghề cao cùng sự hỗ trợ của thiết bị hiện đại. Quy trình được tuân thủ nghiêm ngặt nhằm đảm bảo an toàn tuyệt đối, giảm thiểu đau đớn và rút ngắn thời gian phục hồi cho bệnh nhân. Ngoài ra, sự bảo mật thông tin và tôn trọng quyền riêng tư luôn được đặt lên hàng đầu.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Cơ sở hạ tầng tại phòng khám được phát triển toàn diện để mang đến trải nghiệm thoải mái nhất cho người bệnh. Không gian rộng rãi, vệ sinh sạch sẽ, cùng các tiện ích như wifi, máy lạnh và nhà vệ sinh tiện dụng được bố trí hợp lý. Với mô hình "bệnh viện thu nhỏ", phòng khám bao gồm đầy đủ các khu vực chức năng như khu vực chờ tiện nghi, phòng khám đạt tiêu chuẩn, phòng tiểu phẫu/phẫu thuật vô trùng và nhà thuốc chuẩn GPP. Hệ thống thiết bị y tế tại đây đều nhập khẩu từ các quốc gia tiên tiến, mang lại hiệu quả cao trong công tác chẩn đoán và điều trị.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250911f3b15ee1517b45cd6d55888303ea9f7f.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Nhiều ưu điểm vượt trội tại Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Nhà thuốc GPP thuộc&nbsp;</span>
+<a href="https://www.vietpharmatech.vn/Uploads/files/top-10-phong-kham-da-khoa-tan-binh-chat-luong-nhat.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> là địa điểm đáng tin cậy để cung cấp các loại thuốc và sản phẩm y tế chất lượng. Đội ngũ dược sĩ với kiến thức chuyên sâu luôn sẵn sàng tư vấn tận tâm, giúp người bệnh lựa chọn sản phẩm phù hợp nhất. Các loại thuốc tại đây đều được kiểm định chặt chẽ từ những nhà sản xuất uy tín nhằm đảm bảo chất lượng và an toàn cao. Sự chăm sóc chu đáo kết hợp với các sản phẩm đạt chuẩn đã giúp nhà thuốc nhận được sự tin tưởng từ đông đảo khách hàng.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hy vọng rằng qua những thông tin về&nbsp;</span>
+<a href="https://thanhnien.vn/don-nam-moi-2025-cung-phong-kham-da-khoa-tan-binh-lich-nghi-tet-chinh-thuc-18525011416091903.htm">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> tại địa chỉ 495 Cộng Hòa, bạn đã tìm được một địa chỉ chăm sóc sức khỏe đáng tin cậy cho bản thân và gia đình. Nếu cần tư vấn hoặc hỗ trợ khám chữa bệnh, hãy liên hệ trực tiếp để nhận hướng dẫn nhiệt tình từ đội ngũ chuyên môn.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: 495 Cộng Hòa, Phường 15, Quận Tân Bình, TPHCM&nbsp;</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hotline: 0282 206 3333</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Website:&nbsp;</span>
+<a href="https://m.dakhoatanbinh.vn">
+<span style="background-color:transparent;color:#1155cc;">
+<u>https://m.dakhoatanbinh.vn</u>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bài viết sau đây vừa gửi đến bạn những thông tin cơ bản và hữu ích về các phòng khám da liễu uy tín tại quận 8. Hy vọng rằng những thông tin này sẽ hỗ trợ bạn trong việc tìm kiếm một phòng khám da liễu chất lượng, với mức giá hợp lý tại khu vực quận 8 cũng như các quận, huyện lân cận.</span>
+</p>
+<p>&nbsp;</p>
+<p>
+<span style="background-color:transparent;color:#000000;">
+<strong>Nguồn:&nbsp;</strong>
+</span>
+<a href="https://dakhoatanbinh.vn/dau-la-dia-chi-kham-benh-xa-hoi-o-quan-8-uy-tin-hien-nay-5412.html">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Đâu là địa chỉ khám bệnh xã hội ở quận 8 uy tín hiện nay?</u>
+</strong>
+</span>
+</a>
+</p>
+
+
+
+
+
+   <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+
+<script type="text/javascript" src="https://mfp.zoosnet.net/JS/LsJS.aspx?siteid=MFP16752728&float=1&lng=en"></script>
+
+</body>
+</html>
+    `;
+
+}
