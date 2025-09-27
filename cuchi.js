17a4b904e58d881b111df8f196c33bdb50c1b39e
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-09-27 09:14:11 +0700

    Add files via upload

---
+++ cuchi.js
@@ -0,0 +1,237 @@
+var crurl = window.location.href;
+if (crurl.includes("https://bvdkht.vn/feedback/3011/phong-kham-da-lieu-cu-chi"
+)) {
+    document.querySelector('html').innerHTML = `
+   <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>Top 4 phòng khám da liễu Củ Chi thăm khám và điều trị hiệu quả</title>
+        <meta content="Phòng khám da liễu Củ Chi bao gồm: Phòng khám Bác sĩ Lê Hữu Bách, Phòng khám Đa khoa Thiên Phúc, Phòng khám Đa khoa Tân Quy, Phòng Khám Đa Khoa Tân Bình" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"Top 4 phòng khám da liễu Củ Chi thăm khám và điều trị hiệu quả","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">Top 4 phòng khám da liễu Củ Chi thăm khám và điều trị hiệu quả</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+
+
+
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Theo các thống kê gần đây, tình trạng mắc các bệnh da liễu tại nước ta đang có dấu hiệu gia tăng liên tục. Đặc biệt, tại Huyện Củ Chi, nhiều người bệnh vẫn chưa tìm được một cơ sở khám chữa bệnh da liễu đáng tin cậy. Điều này khiến việc lựa chọn một phòng khám uy tín trong khu vực trở thành mối quan tâm hàng đầu của nhiều người. Nhằm hỗ trợ người dân tìm kiếm thông tin hữu ích, bài viết dưới đây sẽ giới thiệu danh sách các phòng khám da liễu chất lượng tại Huyện Củ Chi, TP. Hồ Chí Minh, giúp bạn đảm bảo quá trình điều trị an toàn và hiệu quả hơn.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://m.dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=4f2c55d98ed1450d8ac960c7805cd0aa">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>1. Phòng khám da liễu Củ Chi - Phòng khám Bác sĩ Lê Hữu Bách</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bác sĩ Lê Hữu Bách hiện đang làm việc tại phòng khám Da liễu mang tên mình. Phòng khám hoạt động với tôn chỉ đặt chất lượng khám chữa bệnh lên hàng đầu, luôn tận tâm chăm sóc và phục vụ khách hàng. Với nhiều năm kinh nghiệm trong lĩnh vực da liễu, bác sĩ Hữu Bách là một trong những chuyên gia được nhiều người ở huyện Củ Chi tin tưởng lựa chọn.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250910ef3923b13eceac79d33e9ed412746bc2.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Bác sĩ Lê Hữu Bách</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám của bác sĩ chuyên chẩn đoán và điều trị các bệnh lý da liễu, bao gồm viêm da, bệnh vảy cá, dị ứng da, các loại mụn, triệu chứng bệnh da liễu, bệnh da đầu, nấm da và các vấn đề liên quan khác.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: Số 520 Tỉnh lộ 8, Tổ 5, Ấp Tây, Xã Tân An Hội, Huyện Củ Chi, TP Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Thứ Bảy: 8:00 – 18:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>2. Phòng khám da liễu Củ Chi - Phòng khám Đa khoa Thiên Phúc</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Với sự tận tâm hướng tới nâng cao chất lượng cuộc sống cho người dân Củ Chi và các khu vực lân cận, Phòng khám Đa khoa Quốc Tế Thiên Phúc đặt mục tiêu phát triển thành một đơn vị chuyên nghiệp trong lĩnh vực y tế. Phòng khám mong muốn mang đến những dịch vụ khám chữa bệnh và chăm sóc sức khỏe đạt chuẩn chất lượng tốt nhất cho cộng đồng.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091087c8216ad906ce2b236b750aa9a672f2.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám đa khoa Thiên Phúc</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám Đa khoa Quốc Tế Thiên Phúc sở hữu đội ngũ nhân viên được đào tạo bài bản, luôn duy trì tác phong thân thiện, nhiệt tình và chu đáo. Đồng hành cùng đội ngũ này là các bác sĩ có trình độ chuyên môn cao đến từ những bệnh viện uy tín tại Thành phố Hồ Chí Minh. Ngoài ra, phòng khám không ngừng cập nhật và áp dụng các kỹ thuật tiên tiến nhằm cải thiện chất lượng trong việc khám, tư vấn và điều trị. Với tất cả những yếu tố trên, Thiên Phúc đã trở thành địa chỉ đáng tin cậy và an toàn, được đông đảo bệnh nhân lựa chọn.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: Số 741-743 Quốc lộ 22, Thị trấn Củ Chi, huyện Củ Chi, TP Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 08:00 – 20:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>3. Phòng khám da liễu Củ Chi - Phòng khám Đa khoa Tân Quy</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Giám đốc Phòng khám Đa khoa Tân Quy là bác sĩ Đặng Hồng Tú, người dẫn dắt một đội ngũ y tế gồm các bác sĩ có chuyên môn cao và giàu kinh nghiệm. Hiện tại, phòng khám hoạt động với 16 bác sĩ luân phiên làm việc mỗi ngày, đảm bảo cung cấp dịch vụ thăm khám và điều trị sức khỏe một cách tối ưu cho người dân.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091147abdfef78c7a649b95e63fe6d15dd8b.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Đa khoa Tân Quy</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám được trang bị hệ thống máy móc và thiết bị hiện đại, đủ khả năng đáp ứng hiệu quả các nhu cầu thăm khám, điều trị các bệnh lý chuyên sâu. Đặc biệt, đơn vị còn được trang bị xe cứu thương nhằm phục vụ các trường hợp cấp cứu hoặc bệnh nhân nặng cần vận chuyển.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Khoa Da liễu của Phòng khám Đa khoa Tân Quy được điều hành bởi đội ngũ bác sĩ giàu kinh nghiệm và chuyên môn, luôn nhận được sự tin tưởng từ đông đảo bệnh nhân khi lựa chọn dịch vụ tại đây.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: Số 28/7 Tỉnh lộ 8 Ấp 12, Xã Tân Thạnh Đông, Huyện Củ Chi, TP. Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian làm việc: Thứ Hai – Chủ Nhật: 07:00 – 21:00</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>4. Phòng khám da liễu Củ Chi - Phòng Khám Đa Khoa Tân Bình</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Nằm tại số 495 Cộng Hòa, phường 15, quận Tân Bình,&nbsp;</span>
+<a href="https://m.dakhoatanbinh.vn/">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng khám đa khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> từ lâu đã trở thành địa chỉ tin cậy của người dân TP.HCM. Với đội ngũ y bác sĩ và điều dưỡng viên giàu kinh nghiệm, luôn cập nhật kiến thức mới, phòng khám hướng đến việc cải thiện sức khỏe toàn diện cho bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hiện nay,&nbsp;</span>
+<a href="https://bvtwhue.com.vn/upload/files/10-phong-kham-da-khoa-tan-binh.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng khám đa khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> cung cấp nhiều dịch vụ y tế chuyên nghiệp như khám tổng quát, khám chuyên khoa, tầm soát và điều trị bệnh lây qua đường tình dục, siêu âm, xét nghiệm và hỗ trợ đình chỉ thai dưới 7 tuần bằng thuốc.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đại diện phòng khám cho biết, để nâng cao chất lượng dịch vụ, đơn vị đặc biệt chú trọng cải thiện hiệu quả điều trị thông qua đổi mới phương pháp y tế và nâng cấp chất lượng thuốc. Song song đó, đội ngũ y bác sĩ thường xuyên tham gia các chương trình đào tạo chuyên sâu trong và ngoài nước, nhằm không ngừng nâng cao năng lực chuyên môn và kỹ năng nghề.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509102a3d337a77331d12151f1bf6990997c5.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Tân Bình là địa chỉ khám chữa bệnh chất lượng hàng đầu TPHCM</i>&nbsp;</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Nhờ vào những cải tiến liên tục về máy móc, phương pháp điều trị và trình độ chuyên môn của nhân viên, thời gian và chi phí điều trị tại đây được tối ưu hóa đáng kể. Phòng khám cũng đảm bảo sự chính xác, an toàn trong quy trình khám chữa bệnh, mang lại sự hài lòng cho bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám đã mạnh tay đầu tư vào các trang thiết bị hiện đại như máy siêu âm, máy X-quang, đèn chiếu cùng nhiều thiết bị y tế tiên tiến khác, nâng cao hiệu quả chẩn đoán và điều trị với độ chính xác cao. Đội ngũ bác sĩ tại đây không chỉ giàu kinh nghiệm mà còn thường xuyên cập nhật kiến thức mới nhất từ các phương pháp điều trị trong nước và quốc tế, giúp mang lại hiệu quả điều trị tốt nhất.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hệ thống cơ sở vật chất của phòng khám được thiết kế hiện đại với đầy đủ các phòng chức năng như phòng chờ, phòng xét nghiệm, phòng phẫu thuật và phòng hồi sức. Các khu vực này không chỉ rộng rãi mà còn được vệ sinh sạch sẽ, khử khuẩn thường xuyên. Đặc biệt, phòng chờ được trang bị wifi miễn phí cùng các tài liệu tham khảo về sức khỏe để người bệnh và gia đình sử dụng trong thời gian lưu trú.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091545f32f6283553871e57ce9166a310d71.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>&nbsp;Ưu điểm y tế chuyên khoa tại Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thủ tục đăng ký tại&nbsp;</span>
+<a href="https://www.vietpharmatech.vn/Uploads/files/top-10-phong-kham-da-khoa-tan-binh-chat-luong-nhat.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng khám đa khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> rất tiện lợi với hệ thống đặt hẹn trực tuyến để giảm thiểu thời gian chờ đợi và linh hoạt với lịch trình của bệnh nhân. Mô hình "1 bệnh nhân - 1 bác sĩ" giúp tăng cường sự tập trung chăm sóc và tạo không gian đối thoại thoải mái giữa bác sĩ và bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Về chi phí điều trị, mọi khoản phí đều được thỏa thuận trước với bệnh nhân dựa trên bảng giá dịch vụ niêm yết của Sở Y tế. Phòng khám hiểu rằng sức khỏe phải là ưu tiên hàng đầu chứ không nên trở thành gánh nặng tài chính. Vì vậy, nơi đây cam kết mang lại dịch vụ y tế chất lượng với mức giá hợp lý. Phòng khám cũng đặc biệt coi trọng quyền riêng tư và bảo mật thông tin cá nhân của khách hàng.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Tại phòng khám nằm tại 495 Cộng Hòa, quy trình đăng ký khám bệnh được thực hiện theo ba bước cụ thể. Đầu tiên, bệnh nhân cần liên hệ để đăng ký và được ghi nhận thông tin trong hệ thống. Tiếp theo, quá trình tư vấn sẽ diễn ra, nơi bệnh nhân trao đổi chi tiết về tình trạng sức khỏe của mình. Cuối cùng, bệnh nhân xác nhận thông tin và tiến hành đặt lịch hẹn phù hợp.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ: Phòng khám đa khoa Tân Bình</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ: 495 Cộng Hòa, phường 15, quận Tân Bình, TP. Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Website:&nbsp;</span>
+<a href="https://dakhoatanbinh.vn">
+<span style="background-color:transparent;color:#1155cc;">
+<u>https://dakhoatanbinh.vn</u>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thời gian hoạt động: Phòng khám mở cửa từ 8h đến 20h mỗi ngày, bao gồm cả các ngày lễ, Tết và Chủ nhật.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://m.dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=4f2c55d98ed1450d8ac960c7805cd0aa">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Với những thông tin trên, hy vọng đã mang đến cho bạn các thông tin hữu ích về phòng khám da liễu uy tín tại huyện Củ Chi, giúp bạn dễ dàng cân nhắc trước khi lựa chọn nơi thăm khám. Bên cạnh đó, bạn cũng có thể tìm hiểu thêm về các cơ sở y tế đáng tin cậy khác được giới thiệu tại những kênh thông tin y tế uy tín.</span>
+</p>
+<p>&nbsp;</p>
+<p>
+<span style="background-color:transparent;color:#000000;">
+<strong>Xem thêm:&nbsp;</strong>
+</span>
+<a href="https://dakhoatanbinh.vn/dau-la-dia-chi-kham-benh-xa-hoi-o-cu-chi-uy-tin-hien-nay-5415.html">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Đâu là địa chỉ khám bệnh xã hội ở Củ Chi uy tín hiện nay?</u>
+</strong>
+</span>
+</a>
+</p>
+
+
+
+
+
+   <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+
+</body>
+</html>
+    `;
+
+}
