ed2be8f376c4dd6b9b8e4d0f53cf033b66ab9ba3
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-09-30 14:54:27 +0700

    Add files via upload

---
+++ quan10.js
@@ -0,0 +1,309 @@
+var crurl = window.location.href;
+if (crurl.includes("https://bvdkht.vn/feedback/3065/phong-kham-da-lieu-quan-10"
+)) {
+    document.querySelector('html').innerHTML = `
+   <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>Top 6 phòng khám da liễu quận 10 khám chữa bệnh an toàn chất lượng</title>
+        <meta content="Phòng khám da liễu quận 10 bao gồm: Phòng khám BS. Võ Thị Bạch Sương, Phòng khám Da liễu BS.CKI Lê Vũ Chí Phước, Phòng khám Da liễu của TS.BS. Châu Văn Trở, Phòng khám Da liễu của BS. Trương Luân, Phòng khám Da Liễu của ThS.BS. Ngô Minh Vinh, Phòng Khám Đa Khoa Tân Bình" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"Top 6 phòng khám da liễu quận 10 khám chữa bệnh an toàn chất lượng","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">Top 6 phòng khám da liễu quận 10 khám chữa bệnh an toàn chất lượng</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+
+
+
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Các bệnh lý da liễu như vảy nến, viêm da cơ địa, lang ben, hoặc các vấn đề như nám, mụn, tàn nhang đang ngày càng phổ biến. Điều này dẫn đến nhu cầu tìm kiếm các phòng khám da liễu uy tín ngày càng tăng. Nếu bạn đang băn khoăn về việc lựa chọn phòng khám da liễu chất lượng tại quận 10, danh sách 6 phòng khám được tổng hợp dưới đây có thể là nguồn tham khảo hữu ích dành cho bạn!</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>1. Phòng khám da liễu quận 10 - Phòng khám BS. Võ Thị Bạch Sương</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám này được biết đến như một địa chỉ uy tín trong lĩnh vực da liễu, được nhiều bệnh nhân tin tưởng lựa chọn. Hiện tại, bác sĩ công tác tại Phòng Chăm sóc da thuộc Bệnh viện Đại học Y Dược TP.HCM và đồng thời đảm nhiệm vai trò giảng viên tại Khoa Da liễu của bệnh viện.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092676483ef2bb35ca4d7f28d87871e3b6d4.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám BS. Võ Thị Bạch Sương&nbsp;</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Khi đến đây, bệnh nhân sẽ nhận được sự tư vấn, thăm khám cũng như điều trị các vấn đề liên quan đến da, lông, móng, tóc và những bệnh lý niêm mạc lưỡi, miệng. Các bệnh xã hội như sùi mào gà, herpes sinh dục hay lậu cũng được chữa trị đảm bảo an toàn và hiệu quả.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ:123 Hòa Hưng, Phường 12, Quận 10, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Phòng khám làm việc từ thứ 2 đến thứ bảy (trừ thứ 5) trong khung giờ từ 14h đến 22h. Chủ nhật phòng khám nghỉ.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>2. Phòng khám da liễu quận 10 - Phòng khám Da liễu BS.CKI Lê Vũ Chí Phước</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Trong số các phòng khám da liễu tại khu vực quận 10, phòng khám của bác sĩ Lê Vũ Chí Phước xứng đáng là một địa chỉ đáng tin cậy mà bạn không thể bỏ qua. Bác sĩ hiện đảm nhận vai trò giảng viên tại Trường Đại học Y khoa Phạm Ngọc Thạch, đồng thời là bác sĩ tại Bệnh viện Da liễu TP.HCM. Ngoài ra, ông còn là giám đốc chuyên môn tại Trung tâm Thẩm mỹ Cosmo và bác sĩ Bệnh viện Đại học Y Dược TP.HCM chuyên khoa phẫu thuật tạo hình và chăm sóc da.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092697fd9899c18246258eff15d943958390.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Da liễu BS.CKI Lê Vũ Chí Phước</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Khi đến thăm khám, bệnh nhân sẽ nhận được sự tư vấn và điều trị tận tình cho các bệnh lý về da, bao gồm các vấn đề như mụn, vết thâm, sẹo, nám, dị ứng hay viêm da. Đội ngũ phòng khám luôn duy trì thái độ vui vẻ, nhiệt tình và tận tâm, cam kết mang lại sự hài lòng tối đa cho bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ: Số 249/29 Nguyễn Tiểu La, Phường 8, Quận 10, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Phòng khám làm việc từ thứ 2 đến thứ 5 khung giờ 8h00 đến 17h00 và thứ sáu từ 8h00 đến 11h. Thứ bảy và chủ nhật thì phòng khám nghỉ.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>3. Phòng khám da liễu quận 10 - Phòng khám Da liễu của TS.BS. Châu Văn Trở</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Một địa chỉ phòng khám da liễu uy tín tại quận 10 được đông đảo bệnh nhân tin tưởng lựa chọn là phòng khám da liễu của TS.BS Châu Văn Trở. Hiện tại, bác sĩ Châu Văn Trở đang giữ vai trò giảng viên tại trường Y khoa Phạm Ngọc Thạch. Với nhiều năm kinh nghiệm và trình độ chuyên môn cao, ông đảm bảo điều trị hiệu quả các bệnh lý về da như dị ứng, nám, chàm, mụn trứng cá, sẹo lồi…</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091153756c0f376e4078224cb80355d9b038.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Da liễu của TS.BS. Châu Văn Trở</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bên cạnh đó, phòng khám còn được đánh giá cao nhờ đội ngũ bác sĩ tận tâm, giàu kinh nghiệm, cùng cơ sở vật chất hiện đại, sạch sẽ và đầy đủ thiết bị. Đây chính là một địa chỉ tin cậy, giúp bệnh nhân yên tâm thăm khám và điều trị các vấn đề về da.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ: Số 142 Ngô Quyền, Phường 5, Quận 10, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Phòng khám làm việc từ thứ 2 đến thứ 7 trong khung giờ 17h00 đến 20h00 Khám bệnh và Nhà thuốc. Chăm sóc da làm việc từ thứ 2 đến thứ 6 trong khung giờ 10h00 đến 20h00. Thứ bảy phòng khám làm việc từ 9h00 đến 20h00 nhiệm vụ chăm sóc da. Chủ nhật cùng những ngày Lễ, ngày Tết thì phòng khám nghỉ.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>4. Phòng khám da liễu quận 10 - Phòng khám Da liễu của BS. Trương Luân</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Nếu vẫn đang băn khoăn không biết tìm địa chỉ phòng khám da liễu quận 10 nào thì phòng khám của BS Trương Luân cũng là địa chỉ được nhiều bệnh nhân chọn lựa. Bác sĩ hiện đang công tác ở khoa Da liễu của BV Y dược TP.HCM.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509116120a4976be3dd8686a909fc5d043c6d.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Da liễu của BS. Trương Luân</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám của bác sĩ được đánh giá uy tín với bác sĩ Luân tay nghề cao, vững vàng và thái độ tiếp đón bệnh nhân ân cần lịch thiệp. Phòng khám được đầu tư trang thiết bị máy móc hiện đại phục vụ nhu cầu khám chữa bệnh cho bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ: 278/16 Tô Hiến Thành, Phường 15, Quận 10, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Phòng khám da liễu làm việc từ thứ 2 đến thứ 5 khung giờ từ 16h00 đến 19h00. Ngoài ra nếu có thăm khám ngoài giờ vui lòng đặt lịch trước để được nhân viên tư vấn sắp xếp thời gian điều trị thuận lợi với mọi người.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>5. Phòng khám da liễu quận 10 - Phòng khám Da Liễu của ThS.BS. Ngô Minh Vinh</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Địa chỉ phòng khám da liễu uy tín chất lượng ở quận 10 cuối cùng mà chúng tôi muốn giới thiệu cùng bạn đó là phòng khám của Ths, BS Ngô Minh Vinh. Bác sĩ chuyên khám tư vấn cũng như giải quyết những vấn đề liên quan đến dị ứng, tàn nhang, sẹo lồi, sẹo lõm, da mụn, rạn…</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250911906e0674e7326c0ae4300057d29cf093.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám Da Liễu của ThS.BS. Ngô Minh Vinh</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Trình độ bác sĩ tay nghề cao, giàu kinh nghiệm kết hợp với trang thiết bị máy móc hiện đại sẽ nhanh chóng khám và điều trị cho bệnh nhân hiệu quả.&nbsp;</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ: 419/1A – B Cách Mạng Tháng 8, Phường 13, Quận 10, Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Phòng khám làm việc khung giờ từ 7h30 đến 17h30 các ngày từ thứ 2 đến chủ nhật.</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#6aa84f;">
+<strong>6. Phòng khám da liễu quận 10 - Phòng Khám Đa Khoa Tân Bình</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<a href="https://m.dakhoatanbinh.vn">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> là địa chỉ đáng tin cậy, nơi bệnh nhân có thể thăm khám và điều trị các bệnh xã hội một cách an toàn, hiệu quả, nhanh chóng mà không mất nhiều thời gian chờ đợi. Với kinh nghiệm thành công trong việc chữa trị từ các trường hợp nhẹ đến nặng, phòng khám ngày càng nhận được sự tin tưởng và lựa chọn của đông đảo người bệnh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Khi đến Phòng Khám Đa Khoa Tân Bình, người bệnh sẽ được trải nghiệm các dịch vụ y tế chất lượng cao và đảm bảo quyền lợi tốt nhất. Cụ thể:</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Môi trường khám chữa bệnh đạt chuẩn</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">&nbsp;&nbsp;Phòng khám được thiết kế khang trang, rộng rãi, sạch sẽ, cùng với khu vực phòng tiểu phẫu đạt tiêu chuẩn vô trùng, mang lại sự thoải mái cho bệnh nhân và giảm thiểu nguy cơ nhiễm trùng.</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Đội ngũ chuyên gia giàu kinh nghiệm</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">&nbsp;&nbsp;Bác sĩ và chuyên gia tại đây đều có trình độ chuyên môn cao, am hiểu sâu về các bệnh xã hội. Đồng thời, đội ngũ y tá và điều dưỡng luôn tận tâm chăm sóc từng bệnh nhân.</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Ứng dụng công nghệ hiện đại&nbsp;&nbsp;</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">&nbsp;&nbsp;Hệ thống máy móc và thiết bị y tế tiên tiến được nhập khẩu, đảm bảo vệ sinh nghiêm ngặt và bảo dưỡng định kỳ để hỗ trợ tối ưu trong việc chẩn đoán và điều trị.</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Bảo mật thông tin&nbsp;</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">&nbsp;&nbsp;Thông tin cá nhân và bệnh án của bệnh nhân được cam kết bảo mật nghiêm ngặt, giúp đảm bảo quyền riêng tư tuyệt đối.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509102a3d337a77331d12151f1bf6990997c5.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Quy trình khám nhanh gọn&nbsp;&nbsp;</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">&nbsp;&nbsp;Quy trình khám chữa bệnh được thiết kế khoa học với thủ tục đơn giản, giúp tiết kiệm thời gian cho người bệnh. Ngoài ra, bệnh nhân có thể đặt lịch hẹn trước thông qua dịch vụ trực tuyến để được sắp xếp khám ngay khi tới.</span>
+</p>
+<h3 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>- Chi phí hợp lý</strong>
+</span>
+</h3>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Mọi khoản phí thăm khám và điều trị đều minh bạch, hợp lý và được tư vấn rõ ràng trước khi bắt đầu điều trị.</span>
+</p>
+<p style="text-align:justify;">
+<a href="https://congan.com.vn/doi-song/suc-khoe/phong-kham-da-khoa-tan-binh-luon-dat-suc-khoe-benh-nhan-len-tren-het_176156.html">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> luôn nỗ lực mang lại dịch vụ y tế chất lượng và sự hài lòng cho mọi bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Thông tin liên hệ:</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Địa chỉ: 495 Cộng Hòa, Phường 15, Quận Tân Bình, TP. Hồ Chí Minh</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Hotline: 0282 206 3333</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">► Thời gian làm việc: Từ 08:00 - 20:00 tất cả các ngày trong tuần, kể cả các dịp Lễ - Tết</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Dựa vào các thông tin liên quan tới vấn đề Đâu là&nbsp;<strong>phòng khám da liễu quận 10</strong> uy tín tại TPHCM? ở trên, chắc hẳn đã giúp người bệnh có được quyết định nên khám chữa bệnh ở đâu uy tín và an toàn, mang lại hiệu quả điều trị cao.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<p>
+<span style="background-color:transparent;color:#000000;">
+<strong>Nguồn:&nbsp;</strong>
+</span>
+<a href="https://dakhoatanbinh.vn/dia-chi-kham-nam-khoa-o-quan-10-uy-tin-noi-nam-gioi-an-tam-lua-chon-5376.html">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Địa chỉ khám nam khoa ở quận 10 uy tín - Nơi nam giới an tâm lựa chọn</u>
+</strong>
+</span>
+</a>
+</p>
+
+
+
+
+
+
+
+
+   <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+
+</body>
+</html>
+    `;
+
+}
